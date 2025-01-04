1d4f2ea307bd5cfca183ad2b5572ad858a9466b2
kanghy <102237457+icedisk2009@users.noreply.github.com>
2025-01-04 10:16:32 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
-st.title('우리나라의 합계 출산율 분석')
-st.write('한국의 출산율은 경제협력개발기구(OECD) 회원국 중 압도적으로 꼴찌다. 지난 2013년부터 11년째 최하위를 기록 중이며, OECD 평균(1.58명)의 절반에도 미치지 못한다. 
-')
+st.title("10년간 대한민국 미세먼지 데이터 분석")
+st.write("한국의 출산율은 경제협력개발기구(OECD) 회원국 중 압도적으로 꼴찌다. 지난 2013년부터 11년째 최하위를 기록 중이며, OECD 평균(1.58명)의 절반에도 미치지 못한다.")
